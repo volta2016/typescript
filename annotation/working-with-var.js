3f57ac8267ab21cbc442cbba1f37af46ed1427aa
Volta <giovoltadesign@gmail.com>
2021-09-04 18:17:48 -0400

    [ts] anonotation ts - of type and inference

---
+++ annotation/working-with-var.js
@@ -0,0 +1,8 @@
+// anotacion de tipo
+var home;
+var dia;
+//inferencia de tipos
+var nameUser;
+// nameUser.toLocaleLowerCase();
+dia = 2;
+console.log(dia);
---
+++ annotation/working-with-var.ts
@@ -0,0 +1,12 @@
+// anotacion de tipo
+let home: string;
+let dia: number;
+
+//inferencia de tipos
+let nameUser: "Kyo";
+
+// nameUser.toLocaleLowerCase();
+
+dia = 2;
+
+console.log(dia);
