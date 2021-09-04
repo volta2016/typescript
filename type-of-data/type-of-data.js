99e90cb88774161b0177b4981a812f31b50fce04
Volta <giovoltadesign@gmail.com>
2021-09-04 16:16:30 -0400

    [ts] type-of-data

--- type-of-data/type-of-data.js
+++ type-of-data/type-of-data.js
@@ -1,23 +1,16 @@
 //los tipos básicos en TS
-
-let number: number;
-let cadena: string;
-let soyBolean: boolean;
-
+var number;
+var cadena;
+var soyBolean;
 //type string
 cadena = "Hola mundo";
-
 console.log(cadena);
-
 //type bolean
 soyBolean = true;
-
 //type numeber > podemos usar valor negativos con decimales no da error
 number = -8.89;
 console.log(number);
-
-function sumaDatos(num: number, num2: number) {
-	return num + num2;
+function sumaDatos(num, num2) {
+    return num + num2;
 }
-
 console.log(sumaDatos(3, 27));
