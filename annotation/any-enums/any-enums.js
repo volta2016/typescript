bff6512e37fdf7643defcadaa44f85e3e6fb3918
Volta <giovoltadesign@gmail.com>
2021-09-11 22:29:21 -0300

    [ts] any definition

---
+++ annotation/any-enums/any-enums.js
@@ -0,0 +1,2 @@
+var severalThing = ["Gato", 3, -4.56, true, null, undefined];
+console.log(severalThing);
---
+++ annotation/any-enums/any-enums.ts
@@ -0,0 +1,7 @@
+//Any is a variable of type any that can contain data of any type, at any time.
+//Any is used to basically skip typing.
+//Any es una variable de tipo any que puede contener datos de cualquier tipo, en cualquier momento
+//Se utiliza para saltarnos el tipado básicamente
+
+let severalThing: any[] = ["Cat", 3, -4.56, true, null, undefined];
+console.log(severalThing);
