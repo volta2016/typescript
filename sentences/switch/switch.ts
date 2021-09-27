4acad2224df5cb3f31674c9e583f4f67e070749b
Volta <giovoltadesign@gmail.com>
2021-09-26 21:30:08 -0300

    [ts] switch

---
+++ sentences/switch/switch.ts
@@ -0,0 +1,32 @@
+//switch
+//se suele utlizar para vereficar multiples valores
+//jecuta un conjunto de declaraciones para cada uno de estos valores
+
+let day: number = 5;
+
+switch (day) {
+	case 1:
+		console.log("Lunes");
+		break;
+	case 2:
+		console.log("Martes");
+		break;
+	case 3:
+		console.log("Miércoles");
+		break;
+	case 4:
+		console.log("Jueves");
+		break;
+	case 5:
+		console.log("Viernes");
+		break;
+	case 6:
+		console.log("Sábado");
+		break;
+	case 7:
+		console.log("Domingo");
+		break;
+	default:
+		console.log("No comtemplado");
+		break;
+}
