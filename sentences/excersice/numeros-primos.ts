40c7ab37a79a39ea4b04904f2289b09e6de66b4a
Volta <giovoltadesign@gmail.com>
2021-10-12 10:43:05 -0300

    [ts] primos

---
+++ sentences/excersice/numeros-primos.ts
@@ -0,0 +1,10 @@
+/*Devolver los primeros numeros pares 
+1- Esto que nos va a decir cuando el resto es 0 el numero es par:
+if (j % 2 == 0) {}
+Cuando hay solo un sentencia lo puedo hacer sin llaves
+*/
+
+for (let j = 0; j < 100; j++) if (j % 2 == 0) console.log(j);
+
+//Los mismo pero menos código para buscar el par
+for (let j = 0; j < 100; j += 2) console.log(j);
