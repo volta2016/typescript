ab89c089f12d7b6ac5af34cdf7b276ef2be5c69f
volta2016 <giovoltadesign@gmail.com>
2023-01-06 19:44:33 -0300

    [ts] practice update 1

--- practice/calculator.ts
+++ practice/calculator.ts
@@ -4,20 +4,24 @@
 //En typescript tu puedes crear tus propios type, puedes exportarlo
 //o importarlo donde lo necesites
 type Operations = "multiply" |  "add" | "divide"  
+type Result = number 
+// type Result = number | string
 
-
-const calculator = (a:number, b:number, op: Operations ): number | string => {
+const calculator = (a:number, b:number, op: Operations ): Result => {
   //en runtime estamos realizando un check
   // if(!Operations.includes(op)) {
   //   console.log("this operation is not define");
   // }
 
   if(op == "multiply") return a * b
-  if(op == "add") return a + b
+  if(op === "add") return a + b
+  
   if(op == "divide") {
-    if (b == 0) return "can´t divide by 0"
+    if (b === 0) throw new Error("can´t divide by 0") 
     return a / b
   }
+
+  throw new Error("Operation is not valid")
 }
 
 //también podemos decir lo que devuelve la función
@@ -27,12 +31,27 @@ const calculator = (a:number, b:number, op: Operations ): number | string => {
 //a operation
 // const operation = await fetch("https://rickandmortyapi.com/api/location").then(res => res.json()).then(res => {return "newLocation"})
 
-const result = calculator(7,2,"add")//esto esta ocurriendo en tiempo
+console.log(calculator(7,2,"add"));//esto esta ocurriendo en tiempo
 //de build time -> en tiempo de compilación
-console.log(result);
+// console.log(calculator(7,2,"dasda"));
+
 
 //cualquier check que hace typescript no corre mientra se ejecuta el código
 //en el cliente
 //OJO no hay ningún checkeo en forma de runtime, eso es un error super común
 //esto funciona: const result = calculator(7,2,"add")
 //por que aquí esta haciendo un check de forma estática
+
+/*
+tenemos el check en runtime que sería esto -> throw new Error("Operation is not valid")
+ y tenemos el check estático que sería el type para evitar cualquier otro op que no exista
+*/
+
+//podemos ocupar try catch
+
+try {
+console.log(calculator(7,2,"add"));
+console.log(calculator(7,0,"divide"));
+} catch(e) {
+  throw new Error("Something went wrong", e)
+}
--- practice/multiplier.ts
+++ practice/multiplier.ts
@@ -1,7 +1,26 @@
+//first example
+// const multiplicator = (a: number, b:number, printText:string) => {
+//   console.log(printText, a*b);
+  
+// }
+
+
+// multiplicator(2,4, "Multiplied a string and 4, the result is")
+
+console.log(process.argv);//lo que nos va a permitir es basicamente poder recuperar los argumentos que le pasamos al comando. Esto es un proceso de node js. Al final siempre typescript tiene que controlar de donde salen las cosas que tipo tiene. Antes lo que tenias que hacer para que tome argv es hacer un 
+//npm install --save dev type/node en este caso no es necesario
+
+//second example
+
 const multiplicator = (a: number, b:number, printText:string) => {
   console.log(printText, a*b);
   
 }
 
 
-multiplicator(2,4, "Multiplied a string and 4, the result is")
+console.log(process.argv);
+
+const a: number = Number(process.argv[2])//tenemos que forzar la transformación a un número
+const b: number = Number(process.argv[3])
+
+multiplicator(a, b, `Multiplied ${a} and ${b} and the result is: `)
